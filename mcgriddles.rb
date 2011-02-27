c6b9e8774e2b966f5c84949c47d222ac15811ff1
Matthias Bauer <moeffju@gmail.com>
2011-02-28 00:14:13 +0100

    Add hexdump

--- mcgriddles.rb
+++ mcgriddles.rb
@@ -5,18 +5,37 @@ require "bundler/setup"
 
 require "em-proxy"
 
+class String
+  def hexdump
+    rr = ''
+    address = 0
+    self.scan(/.{1,16}/m).each do |s|
+      e = s.enum_for :each_byte
+      rr += "%04x  %-#{0x10*3}s |%s|\n" % [ address,
+        e.map{ |b|  "%02x" % b }.join(" "),
+        e.map{ |b|
+          0x20 > b || 0x7f < b  ? "." : b.chr
+        }.join ]
+      address += 0x10
+    end
+    rr
+  end
+end
+
 Proxy.start(:host => "127.0.0.1", :port => 25565, :debug => false) do |conn|
   conn.server :srv, :host => "minecraft.lolcalhost.de", :port => 25565
 
   # modify / process request stream
   conn.on_data do |data|
-    p [:on_data, data]
+    puts ">>>"
+    puts data.hexdump
     data
   end
  
   # modify / process response stream
   conn.on_response do |backend, resp|
-    p [:on_response, backend, resp]
+    puts "<<<"
+    puts resp.hexdump
     resp
   end  
 end
